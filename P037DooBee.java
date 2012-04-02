1726e21d95465cba8683d3b25936e402fd44f4f0
Łukasz Siwiński <lsiwinski@gmail.com>
2012-04-02 22:28:28 +0200

    p. 37: DooBee after filling the empty places (sharpen a pencil)

--- P037DooBee.java
+++ P037DooBee.java
@@ -1,13 +1,14 @@
+// expected result: DooBeeDooBeeDo
 public class P037DooBee {
 
 	public static void main(String args[]){
 		int x=1;
-		while(x<____){
+		while(x<3){
 			System.out.print("Doo");
 			System.out.print("Bee");
 			x=x+1;
 		} 
-		if(x==____){
+		if(x==3){
 			System.out.print("Do");
 		}
 	}
