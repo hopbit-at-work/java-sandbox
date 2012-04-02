774b560e691d7abf48eb9e991960d5f423f82e7b
Łukasz Siwiński <lsiwinski@gmail.com>
2012-04-02 22:24:40 +0200

    p. 37: doobee before filling the empty places (sharpen a pencil)

---
+++ P037DooBee.java
@@ -0,0 +1,14 @@
+public class P037DooBee {
+
+	public static void main(String args[]){
+		int x=1;
+		while(x<____){
+			System.out.print("Doo");
+			System.out.print("Bee");
+			x=x+1;
+		} 
+		if(x==____){
+			System.out.print("Do");
+		}
+	}
+}
--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ javac P035SimpleLogicTests.java
 javac P036WhileLoopExamples.java
 javac P037TestIf1.java
 javac P037TestIf2.java
+javac P037DooBee.java
