4075e5fa5f7ece2a01ff9f69055795ae4bf1abf2
Łukasz Siwiński <lsiwinski@gmail.com>
2012-04-02 23:38:32 +0200

    fixed version of 99 bottles of beer

--- P038PiosenkaOPiwie.java
+++ P038PiosenkaOPiwie.java
@@ -7,19 +7,18 @@ public class P038PiosenkaOPiwie {
 			if(iloscButelek==1){
 				slowo = "bottle";
 			}
-			System.out.println(
+			System.out.print(
 				iloscButelek + 
 				" " + 
 				slowo + 
-				" of beer on the wall");
+				" of beer on the wall, ");
 			System.out.println(iloscButelek + 
 				" " + 
 				slowo + 
 				" of beer.");
-			System.out.println("Take one down.");
-			System.out.println("Pass it around.");
+			System.out.print("Take one down. ");
+			System.out.print("Pass it around.");
 			iloscButelek = iloscButelek - 1;
-			
 			if(iloscButelek>0){
 				System.out.println(iloscButelek + 
 					" " + 
@@ -27,7 +26,10 @@ public class P038PiosenkaOPiwie {
 					" of beer on the wall");
 			} else {
 				System.out.println("No more bottles of beer on the wall");
+				System.out.println("No more bottles of beer");
+				System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall");
 			} // koniec else 
+			System.out.println("");
 		} // koniec while
 	} // koniec main
 }
