b70b4bbd9d8c5ca2d8bf4dc461b7d27ad0bc5c84
Łukasz Siwiński <lsiwinski@gmail.com>
2012-04-06 21:17:55 +0200

    p.45; cwiczenie 1c - rozwiazanie; wystarczylo dodac metode 'main' i dziala

--- P045Cwiczenie1C.java
+++ P045Cwiczenie1C.java
@@ -1,9 +1,11 @@
-public class Template {
+public class P045Cwiczenie1C {
+  public static void main(String[] args){
   int x = 5;
   while(x>1){ 
     x=x-1;
     if(x<3){
       System.out.println("Malutkie x.");
     }
+  }
   }
 }
