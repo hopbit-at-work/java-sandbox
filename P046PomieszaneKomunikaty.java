3d824b592eeb6ce0e2eb87db9c54b87c29ed007c
Łukasz Siwiński <lsiwinski@gmail.com>
2012-04-10 22:53:24 +0200

    p.46; pomieszane przyklady; czwarty blok kodu

--- P046PomieszaneKomunikaty.java
+++ P046PomieszaneKomunikaty.java
@@ -1,45 +1,55 @@
 public class P046PomieszaneKomunikaty {
 
   public static void main(String args[]){
-    int x=0;
-    int y=0;
+
+    int x=0; int y=0;
     while(x<5){
       y=x-y; // 00 11 21 32 42
       System.out.print(x+""+y+" ");
       x=x+1;
     }
-    x=0;y=0;
     System.out.println("");
+
+    x=0;y=0;
     while(x<5){
       y = y+x; // 00 11 23 36 410
       System.out.print(x+""+y+" ");
       x=x+1;
     }
-      //
-      // 3. y=y+2;
-      //    if(y>4){
-      //      y=y-1;
-      //    }
-      // 4. x = x+1;
-      //    y = y+x;
-      //
-      // 5. if(y<5){
-      //      x=x+1;
-      //      if(y<3){
-      //        x=x-1;
-      //      }
-      //    }
-      //    y=y+2;
-      //
-      //    generowane wyniki:
-      //    a. 22 46
-      //    b. 11 34 59
-      //    c. 02 14 26 38
-      //    d. 02 14 36 48
-      //    e. 00 11 21 32 42
-      //    f. 11 21 32 42 53
-      //    g. 00 11 23 36 410
-      //    h. 02 14 25 36 47 
+    System.out.println("");
+
+    x=0;y=0;
+    while(x<5){
+      y=y+2; // 02 14 25 36 47
+      if(y>4){
+        y=y-1;
+      }
+      System.out.print(x+""+y+" ");
+      x=x+1;
+    }
+    System.out.println("");
+
+    x=0;y=0;
+    while(x<5){
+      x=x+1;
+      y=y+x;
+      System.out.print(x+""+y+" "); // 11 34 59 
+      x=x+1;
+    }
+    System.out.println("");
+/*
+    x=0;y=0;
+    while(y<5){
+      if(y<5){
+        x=x+1;
+        if(y<3){
+          x=x-1;
+        }
+      }
+      System.out.print(x+""+y+" "); // 
+      x=x+1;
+    }
+    System.out.println("");*/
 
   }
 }
