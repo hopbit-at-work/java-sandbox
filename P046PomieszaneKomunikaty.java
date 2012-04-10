f173e2466b01953079cd75682799887b5ee9f37e
Łukasz Siwiński <lsiwinski@gmail.com>
2012-04-10 22:33:46 +0200

    p.46, Pomieszane komunikaty; drugi blok kodu

--- P046PomieszaneKomunikaty.java
+++ P046PomieszaneKomunikaty.java
@@ -4,15 +4,17 @@ public class P046PomieszaneKomunikaty {
     int x=0;
     int y=0;
     while(x<5){
-      
       y=x-y; // 00 11 21 32 42
       System.out.print(x+""+y+" ");
       x=x+1;
     }
-    //System.out.println("");
-    //while(x<5){
-    //  y = y+x;
-    //}
+    x=0;y=0;
+    System.out.println("");
+    while(x<5){
+      y = y+x; // 00 11 23 36 410
+      System.out.print(x+""+y+" ");
+      x=x+1;
+    }
       //
       // 3. y=y+2;
       //    if(y>4){
