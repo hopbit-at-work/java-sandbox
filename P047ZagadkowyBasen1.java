441e46e70b26723befe8ef767435ce6c490c0105
Łukasz Siwiński <lsiwinski@gmail.com>
2012-04-23 22:00:59 +0200

    p.47, excercise 'Zagadkowy basen'

--- P047ZagadkowyBasen1.java
+++ P047ZagadkowyBasen1.java
@@ -1,11 +1,26 @@
-class ZagadkowyBasen1 {
-  public static void main(String[] args){
+class P047ZagadkowyBasen1 {
+  public static void main(String[] args)
+  {
     int x=0;
     while(x<4){
       System.out.print("a");
       if(x<1) {
-
+        System.out.print(" ");
       }
+      System.out.print("n");
+      if(x<4){
+        Sytem.out.print("asz");
+        x=x+1;
+      }
+
+      if(x==1){
+        System.out.print("tałek");
+      }
+      if(____){
+        System.out.print("tyczny");
+      }
+      System.out.println("");
+      _______
     }
   }
 }
