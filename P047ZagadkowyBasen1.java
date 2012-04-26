e5d471dbde968a4a04cdaf20c2b23139d84bf3a9
Łukasz Siwiński <lsiwinski@gmail.com>
2012-04-26 22:15:31 +0200

    p. 47, excercise 'Zagadkowy basen' resolved

--- P047ZagadkowyBasen1.java
+++ P047ZagadkowyBasen1.java
@@ -1,26 +1,31 @@
 class P047ZagadkowyBasen1 {
+  /**
+  * Should show:
+  * a nasz
+  * antalek
+  * antyczny
+  */
   public static void main(String[] args)
   {
     int x=0;
-    while(x<4){
-      System.out.print("a");
+    while(x<4){ // 1
+      System.out.print("a"); 
       if(x<1) {
         System.out.print(" ");
       }
-      System.out.print("n");
-      if(x<4){
-        Sytem.out.print("asz");
-        x=x+1;
+      System.out.print("n"); // 4
+      if(x>1){ // 5
+        System.out.print("tyczny"); // 10
+        x=x+2; // 7
       }
-
-      if(x==1){
-        System.out.print("tałek");
+      if(x==1){ 
+        System.out.print("talek"); // 8
       }
-      if(____){
-        System.out.print("tyczny");
+      if(x<1){ // 9
+        System.out.print("asz"); // 6
       }
       System.out.println("");
-      _______
+      x = x+1; // 11
     }
   }
 }
