7738f33e6b137e1121758be2da61b2c68a9fbc3c
Łukasz Siwiński <lsiwinski@gmail.com>
2012-04-30 19:07:25 +0200

    p.60, klasa Pies i klasa testujaca Psa

---
+++ P060Pies.java
@@ -0,0 +1,11 @@
+class P060Pies {
+  // skladowe
+  int wielkosc;
+  String rasa;
+  String nazwa;
+
+  // metoda
+  void szczekaj(){
+    System.out.println("Chau! Chau!");
+  }
+}
---
+++ P060PiesTester.java
@@ -0,0 +1,9 @@
+class P060PiesTester{
+  public static void main(String[] args){
+    // Tutaj umiescimy kod testujacy 
+    // klase P060Pies
+    P060Pies p = new P060Pies();
+    p.wielkosc = 40;
+    p.szczekaj();
+  }
+}
