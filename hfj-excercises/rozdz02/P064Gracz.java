05baa6ab5039aec43f9b292c4d50629a993b5400
default_git_name <default_git_email>
2012-05-25 05:59:08 +0000

    p.64: klasa Gracz

---
+++ hfj-excercises/rozdz02/P064Gracz.java
@@ -0,0 +1,8 @@
+class P064Gracz {
+	int liczba = 0; // tu jest zapisywana typowana liczba
+
+	public void zgaduj(){
+		liczba = (int) (Math.random()*10);
+		System.out.println("Typuje liczbe: "+liczba);
+	}
+}
