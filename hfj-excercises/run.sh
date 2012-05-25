8a92b0bcf0777463f0d47f026fa566474d2b3de6
default_git_name <default_git_email>
2012-05-25 15:55:06 +0000

    now i can run excercises on android using crazy, sexy & marvelous run.sh, script

---
+++ hfj-excercises/run.sh
@@ -0,0 +1,8 @@
+#Run script for the demo console app
+
+#cd into the home directory - so you can run the script from vim or anywhere else
+cd ~/dev/workspace/java/sandbox/hfj-excercises/
+
+#Notice that the DEX version of the .jar is used with java
+java -jar ./dist/$1.dex.jar hfj.r02.$1
+
--- hfj-excercises/src/hfj/r02/runAndroid.sh
+++
@@ -1,3 +0,0 @@
-#Run script for hello world
-java -v -jar $1.jar $1
-
