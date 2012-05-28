a8bad88ffac2080908984985f87042479aa0ef4b
default_git_name <default_git_email>
2012-05-28 16:22:30 +0000

    p.66: ex. A, klasa Magnetofon

---
+++ hfj-excercises/src/hfj/r02/Magnetofon.java
@@ -0,0 +1,9 @@
+class Magnetofon{
+	boolean mozeNagrywac=false;
+	void odtworzTasme(){
+		System.out.println("odtwarzam tasme");
+	}
+	void nagrajTasme(){
+		System.out.println("nagrywam tasme");
+	}
+}
