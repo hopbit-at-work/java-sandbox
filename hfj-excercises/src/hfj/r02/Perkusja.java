ea43e6833179fbcab5c2965acc857a7a645d8538
default_git_name <default_git_email>
2012-05-31 06:05:28 +0000

    p.67: magnesiki z kodem - rozwiazanie

--- hfj-excercises/src/hfj/r02/Perkusja.java
+++ hfj-excercises/src/hfj/r02/Perkusja.java
@@ -1,4 +1,11 @@
+package hfj.r02;
 class Perkusja {
 	boolean beben=true;
 	boolean talerze=true;
+	void zagrajNaBebnie(){
+		System.out.println("bam, bam, baaaa-am-am");
+	}
+	void zagrajNaTalerzach(){
+		System.out.println("brzdek, brzzzzzdek");
+	}
 }
--- hfj-excercises/src/hfj/r02/PerkusjaTester.java
+++ hfj-excercises/src/hfj/r02/PerkusjaTester.java
@@ -1,5 +1,12 @@
+package hfj.r02;
 class PerkusjaTester{
 	public static void main(String[] args){
-
+		Perkusja p = new Perkusja();
+		p.zagrajNaBebnie();
+		p.beben=false;
+		if(p.beben==true){
+			p.zagrajNaBebnie();
+		}
+		p.zagrajNaTalerzach();
 	}
 }
