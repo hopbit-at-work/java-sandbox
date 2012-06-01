285e3a9cf6a489544730254954bc66898bcb8864
default_git_name <default_git_email>
2012-06-01 06:00:47 +0000

    p.68: zagadkowy basen rozwiazany, teraz wynik programu zgadza sie z oczekiwanym

--- hfj-excercises/src/hfj/r02/EchoTester.java
+++ hfj-excercises/src/hfj/r02/EchoTester.java
@@ -4,14 +4,14 @@ public class EchoTester{
 		Echo e1=new Echo();
 		Echo e2=new Echo();
 		int x=0;
-		while(x<5){
+		while(x<4){
 			e1.witaj();
-			e1.ilosc=e1.ilosc+1;
-			if(x<5){
-				e2.ilosc=e2.ilosc+1;
+			e1.ilosc=e1.ilosc+1;// 1,2,3,4
+			if(x==3){
+				e2.ilosc=e2.ilosc+1; // 0, 6
 			}
-			if(x==4){
-				e2.ilosc=e2.ilosc+e1.ilosc;
+			if(x>0){
+				e2.ilosc=e2.ilosc+e1.ilosc; // 2, 5, 10
 			}
 			x=x+1;
 		} // while
