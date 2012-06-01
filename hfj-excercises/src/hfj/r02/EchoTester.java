356c06eb274427e3c50feb995ca3897599320301
default_git_name <default_git_email>
2012-06-01 15:29:42 +0000

    p.68: hmm dziwne... powinno wypisac liczbe 24 a wypisuje co najwyzej 20

--- hfj-excercises/src/hfj/r02/EchoTester.java
+++ hfj-excercises/src/hfj/r02/EchoTester.java
@@ -2,21 +2,21 @@ package hfj.r02;
 /*
  * ta wersja wypisze:
  * 4 x siemasz
- * i liczbe 10
+ * i liczbe 24
  */
 public class EchoTester{
 	public static void main(String[] args){
 		Echo e1=new Echo();
 		Echo e2=new Echo();
 		int x=0;
-		while(x<4){
+		while(x<5){
 			e1.witaj();
-			e1.ilosc=e1.ilosc+1;// 1,2,3,4
-			if(x==3){
-				e2.ilosc=e2.ilosc+1; // 0, 6
+			e1.ilosc=e1.ilosc+1; // 1,2,3,4
+			if(x<5){
+				e2.ilosc=e2.ilosc+1; // 1,3,6,10 
 			}
-			if(x>0){
-				e2.ilosc=e2.ilosc+e1.ilosc; // 2, 5, 10
+			if(x<5){
+				e2.ilosc=e2.ilosc+e1.ilosc; // 2,5,9,24 
 			}
 			x=x+1;
 		} // while
