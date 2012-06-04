03103b4941f0c02684f11445153a5dab8cfe516f
default_git_name <default_git_email>
2012-06-04 05:58:54 +0000

    p.68: rozwizanie pytania dodatkowego, teraz zamiast 10 wyswietla sie 24

--- hfj-excercises/src/hfj/r02/EchoTester.java
+++ hfj-excercises/src/hfj/r02/EchoTester.java
@@ -2,21 +2,21 @@ package hfj.r02;
 /*
  * ta wersja wypisze:
  * 4 x siemasz
- * i liczbe 10
+ * i liczbe 24
  */
 public class EchoTester{
 	public static void main(String[] args){
 		Echo e1=new Echo();
-		Echo e2=new Echo();
+		Echo e2=e1;
 		int x=0;
-		while(x<4){
+		while(x<4){ // 0,1,2,3
 			e1.witaj();
-			e1.ilosc=e1.ilosc+1;
-			if(x==3){
-				e2.ilosc=e2.ilosc+1;
+			e1.ilosc=e1.ilosc+1; // 1,2,5,11
+			if(x==3){ // 0
+				e2.ilosc=e2.ilosc+1; // 12
 			}
-			if(x>0){
-				e2.ilosc=e2.ilosc+e1.ilosc;
+			if(x>0){ // 0
+				e2.ilosc=e2.ilosc+e1.ilosc; // 4, 10, 24
 			}
 			x=x+1;
 		} // while
