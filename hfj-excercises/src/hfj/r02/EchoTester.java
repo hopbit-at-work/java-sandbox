28ca514f26bd8d31f4e76e9d0f0e46aedc957feb
default_git_name <default_git_email>
2012-06-04 05:50:05 +0000

    p.68: powrot do wersji wyswietlajacej 10

--- hfj-excercises/src/hfj/r02/EchoTester.java
+++ hfj-excercises/src/hfj/r02/EchoTester.java
@@ -2,21 +2,21 @@ package hfj.r02;
 /*
  * ta wersja wypisze:
  * 4 x siemasz
- * i liczbe 24
+ * i liczbe 10
  */
 public class EchoTester{
 	public static void main(String[] args){
 		Echo e1=new Echo();
 		Echo e2=new Echo();
 		int x=0;
-		while(x<5){
+		while(x<4){
 			e1.witaj();
-			e1.ilosc=e1.ilosc+1; // 1,2,3,4
-			if(x<5){
-				e2.ilosc=e2.ilosc+1; // 1,3,6,10 
+			e1.ilosc=e1.ilosc+1;
+			if(x==3){
+				e2.ilosc=e2.ilosc+1;
 			}
-			if(x<5){
-				e2.ilosc=e2.ilosc+e1.ilosc; // 2,5,9,24 
+			if(x>0){
+				e2.ilosc=e2.ilosc+e1.ilosc;
 			}
 			x=x+1;
 		} // while
