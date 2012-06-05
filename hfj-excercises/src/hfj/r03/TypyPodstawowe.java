188d6ae2caf1c96d897ae938b82473cfefa7933c
default_git_name <default_git_email>
2012-06-05 14:55:28 +0000

    p.75: typy podstawowe - program demo

---
+++ hfj-excercises/src/hfj/r03/TypyPodstawowe.java
@@ -0,0 +1,19 @@
+package hfj.r03;
+class TypyPodstawowe{
+	public static void main(String[] args){
+		int x;
+		x=234;
+		System.out.println("int x: "+x);
+		byte b=89;
+		System.out.println("byte b: "+b);
+		boolean jestCzadowo=true;
+		System.out.println("boolean jestCzadowo: "+jestCzadowo);
+		double d=3243.98;
+		System.out.println("double d: "+d);
+		char c='f';
+		System.out.println("char c: "+c);
+		int z=x;
+		System.out.println("int z: "+z);
+	// dokonczyc...
+	}
+}
