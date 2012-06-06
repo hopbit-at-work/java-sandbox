1436fcb899b7193d514a3aaaf34eb17f583c0f77
default_git_name <default_git_email>
2012-06-06 15:09:39 +0000

    // wykomentowalem niekompilujace sie klasy

--- hfj-excercises/src/hfj/r03/ZaostrzOlowek.java
+++ hfj-excercises/src/hfj/r03/ZaostrzOlowek.java
@@ -1,10 +1,10 @@
 package hfj.r03;
 class ZaostrzOlowek{
 	public static void main(String[] args){
-		int x=34.5; // err
-		System.out.println("x:"+x);
-		boolean boo=x;//error
-		System.out.println("boo:"+boo);
+		//int x=34.5; // err
+		//System.out.println("x:"+x);
+		//boolean boo=x;//error
+		//System.out.println("boo:"+boo);
 		int g=17; // ok
 		System.out.println("g: "+g);
 		int y=g; // ok
@@ -12,12 +12,12 @@ class ZaostrzOlowek{
 		y=y+10; // ok
 		System.out.println("y: "+y);
 		short s; // ok
-		s=y; // err
+		//s=y; // err
 		byte b=3;//ok
 		byte v=b;//ok
 		short n=12; //ok
-		v=n; //err
-		byte k=128;//err
+		//v=n; //err
+		//byte k=128;//err
 		int p=3*g+y;//ok
 	}
 }
