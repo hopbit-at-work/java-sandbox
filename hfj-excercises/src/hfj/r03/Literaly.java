725f0075ec7c2ddf94d88740b000de3f3c66e4ca
default_git_name <default_git_email>
2012-06-06 05:56:04 +0000

    p.76: literaly

---
+++ hfj-excercises/src/hfj/r03/Literaly.java
@@ -0,0 +1,12 @@
+package hfj.r03;
+class Literaly{
+	public static void main(String[] args){
+		int rozmiar=32; // 32 - Literaly
+		char pierwsza='j';
+		double d=3242.34;
+		boolean czyToWariat;
+		czyToWariat=true;
+		int x=56;
+		int y=x+456;
+	}
+}
