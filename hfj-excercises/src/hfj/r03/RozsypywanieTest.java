8f90456ee331f8e6d56888d90bfa9a917f0adcd5
default_git_name <default_git_email>
2012-06-06 05:38:53 +0000

    p.76: test rozsypywania

--- hfj-excercises/src/hfj/r03/RozsypywanieTest.java
+++ hfj-excercises/src/hfj/r03/RozsypywanieTest.java
@@ -1,5 +1,8 @@
+package hfj.r03;
 class RozsypywanieTest{
 	public static void main(String[] args){
-		// code
+		int x=24;
+		// byte b=x; // to nie przejdzie
+		// kompilator tego nie przepusci
 	}
 }
