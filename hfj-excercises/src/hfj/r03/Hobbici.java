596943f7a0b5b20f0d7ae2164d787f1c2b3eec4c
Łukasz Siwiński <lsiwinski@gmail.com>
2012-06-11 18:43:59 +0200

    p.87: druga wersja cwiczenia b

--- hfj-excercises/src/hfj/r03/Hobbici.java
+++ hfj-excercises/src/hfj/r03/Hobbici.java
@@ -3,8 +3,10 @@
 /**
  * Pierwsza wersja programu
  *
- * Ten program powinien skopmilowac sie 
- * i uruchomic bez problemu;
+ * Program faktycznie udało sie 
+ * skopilowac, ale polecial 
+ * IndexOutOfBoundException :/ 
+ * poprawilem, teraz powinno juz dzialac
  *
  * Wyniki jakie powinien dac kolejno to :
  * Frodo....
@@ -15,7 +17,7 @@
 class Hobbici{
   String imie;
   public static void main(String[] args){
-    Hobbici[] h = new Hobbici[3];
+    Hobbici[] h = new Hobbici[4];
     int z=0;
     while(z<4){
       z=z+1;
