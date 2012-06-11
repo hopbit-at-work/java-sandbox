12ab4a94a3b52ebd7b7b4fdc36c7f84acc5c978b
default_git_name <default_git_email>
2012-06-11 14:39:18 +0000

    usprawnilem skrypt generujacy klasy - jesli user nie podam nazwy klasy, zostanie wyswietlony komunikat informujacy o bledzie

--- hfj-excercises/src/hfj/r03/_gen-main.sh
+++ hfj-excercises/src/hfj/r03/_gen-main.sh
@@ -1,3 +1,5 @@
+[ $# -eq 0 ] && { echo "Usage: $0 class_name"; exit 1; }
+
 echo "package hfj.r03;" >> $1.java
 echo "class "$1"{" >> $1.java
 echo "	public static void main(String[] args){" >> $1.java
